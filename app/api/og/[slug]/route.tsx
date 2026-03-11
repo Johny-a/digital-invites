f3a5465615ab15b882f1e70fd7c4b4f654893e1e
Johnny Aoun <johnyelioyt@gmail.com>
2026-03-11 13:27:01 +0200

    fix og image rendering

--- app/api/og/[slug]/route.tsx
+++ app/api/og/[slug]/route.tsx
@@ -21,59 +21,54 @@ console.log("EVENT:", data);
   const names = data?.hero_names || "Wedding Invitation";
   const photo = data?.ending_photo;
 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          width: "1200px",
-          height: "630px",
-          display: "flex",
-          position: "relative",
-          background: "#000",
-          color: "white",
-        }}
-      >
-        {/* Background Photo */}
-        {photo && (
-          <img
-            src={photo}
-            style={{
-              position: "absolute",
-              inset: 0,
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-            }}
-          />
-        )}
-
-        {/* Dark Overlay */}
-        <div
+return new ImageResponse(
+  (
+    <div
+      style={{
+        width: "1200px",
+        height: "630px",
+        position: "relative",
+        display: "flex",
+        alignItems: "flex-end",
+        justifyContent: "center",
+        color: "white",
+        background: "black",
+      }}
+    >
+      {photo && (
+        <img
+          src={photo}
+          width="1200"
+          height="630"
           style={{
             position: "absolute",
-            inset: 0,
-            background: "rgba(0,0,0,0.35)",
+            top: 0,
+            left: 0,
+            width: "1200px",
+            height: "630px",
+            objectFit: "cover",
           }}
         />
+      )}
 
-        {/* Names */}
-        <div
-          style={{
-            position: "absolute",
-            bottom: 80,
-            width: "100%",
-            textAlign: "center",
-            fontSize: 72,
-            fontWeight: 600,
-          }}
-        >
-          {names}
-        </div>
+      <div
+        style={{
+          position: "relative",
+          fontSize: 72,
+          fontWeight: 600,
+          marginBottom: 60,
+          background: "rgba(0,0,0,0.4)",
+          padding: "20px 40px",
+          borderRadius: 20,
+        }}
+      >
+        {names}
       </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-    }
-  );
+    </div>
+  ),
+  {
+    width: 1200,
+    height: 630,
+  }
+);
 }
