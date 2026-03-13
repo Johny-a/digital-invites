8ae9731a5483bf819b011ec43d391f142fa9ec66
Johnny Aoun <johnyelioyt@gmail.com>
2026-03-13 11:03:03 +0200

    fix og slug and upgrade preview

--- app/[slug]/opengraph-image.tsx
+++ app/[slug]/opengraph-image.tsx
@@ -18,61 +18,78 @@ export default async function Image({
 }: {
   params: { slug: string };
 }) {
-
+const slug = decodeURIComponent(params.slug);
   const { data } = await supabase
     .from("events")
     .select("*")
-    .eq("slug", params.slug)
+    .eq("slug", slug)
     .maybeSingle();
 
   const names = data?.hero_names || "Wedding Invitation";
   const photo = data?.ending_photo;
 
-  return new ImageResponse(
-    (
+return new ImageResponse(
+  (
+    <div
+      style={{
+        width: "1200px",
+        height: "630px",
+        position: "relative",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "black",
+        color: "white",
+        flexDirection: "column",
+      }}
+    >
+      {photo && (
+        <img
+          src={photo}
+          style={{
+            position: "absolute",
+            width: "1200px",
+            height: "630px",
+            objectFit: "cover",
+            top: 0,
+            left: 0,
+          }}
+        />
+      )}
+
       <div
         style={{
-          width: "1200px",
-          height: "630px",
-          display: "flex",
-          alignItems: "flex-end",
-          justifyContent: "center",
-          background: "black",
-          color: "white",
-          position: "relative",
+          position: "absolute",
+          inset: 0,
+          background: "rgba(0,0,0,0.45)",
+        }}
+      />
+
+      <div
+        style={{
+          fontSize: 70,
+          fontWeight: 600,
+          textAlign: "center",
+          zIndex: 10,
         }}
       >
-        {photo && (
-          <img
-            src={photo}
-            style={{
-              position: "absolute",
-              width: "1200px",
-              height: "630px",
-              objectFit: "cover",
-              top: 0,
-              left: 0,
-            }}
-          />
-        )}
-
-        <div
-          style={{
-            fontSize: 72,
-            fontWeight: 600,
-            marginBottom: 80,
-            background: "rgba(0,0,0,0.5)",
-            padding: "20px 40px",
-            borderRadius: 20,
-          }}
-        >
-          {names}
-        </div>
+        {names}
       </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-    }
-  );
-}
+
+      <div
+        style={{
+          fontSize: 32,
+          marginTop: 20,
+          opacity: 0.9,
+          zIndex: 10,
+        }}
+      >
+        You're invited
+      </div>
+    </div>
+  ),
+  {
+    width: 1200,
+    height: 630,
+  }
+);
