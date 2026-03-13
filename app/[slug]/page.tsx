d6d896e8be2bf749d4fccbae761456babd6746f0
Johnny Aoun <johnyelioyt@gmail.com>
2026-03-13 11:22:59 +0200

    use ending photo for OG preview

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -23,17 +23,20 @@ export async function generateMetadata({ params }: { params: { slug: string } })
   return {
     title,
     description: `You're invited to the wedding of ${title}`,
+
     openGraph: {
       title,
       description: "Tap to view the invitation",
+      url: `https://digital-invites-xi.vercel.app/${slug}`,
       images: [
         {
           url: image,
           width: 1200,
           height: 630,
-        },
+        }
       ],
     },
+
     twitter: {
       card: "summary_large_image",
       title,
